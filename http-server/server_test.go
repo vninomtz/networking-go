073f528664ca12c2adfaf1af844d968c61d238e2
Victor Niño <ninomtz.victor@gmail.com>
2025-09-26 00:34:59 -0600

    test

---
+++ http-server/server_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestSimpleHTTPServer(t *testing.T) {
+	srv := &http.Server{
+		Addr:              "127.0.0.1:8081",
+		Handler:           http.TimeoutHandler(nil, 2*time.Minute, ""),
+		IdleTimeout:       5 * time.Minute,
+		ReadHeaderTimeout: time.Minute,
+	}
+
+	l, err := net.Listen("tcp", srv.Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		err := srv.Serve(l)
+		if err != http.ErrServerClosed {
+			t.Error(err)
+		}
+	}()
+}
